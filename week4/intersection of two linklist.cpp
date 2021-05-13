cf51facec6f2599d640aad1c7f159d1515c163cb
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-13 13:57:07 +0530

    Update intersection of two linklist.cpp

--- week4/intersection of two linklist.cpp
+++ week4/intersection of two linklist.cpp
@@ -40,3 +40,41 @@ public:
     }
 };
 method 2:
+brute force
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB)return NULL;
+        else
+        {
+            ListNode* curr1=headA;
+            ListNode* curr2=headB;
+            
+            while(curr1)
+            { curr2=headB;
+                 while(curr2)
+            {
+                if(curr2==curr1)
+                {
+                    return curr2;
+                }
+                 curr2=curr2->next;    
+            }
+            curr1=curr1->next;
+            }
+            return NULL; 
+                
+        }
+        
+    }
+};
+method 3:
+
