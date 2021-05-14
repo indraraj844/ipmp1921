7e804931a2c6cbabfaebe1f462c4154702fa10b1
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-14 12:07:26 +0530

    Update least missing positive number.cpp

--- week4/least missing positive number.cpp
+++ week4/least missing positive number.cpp
@@ -17,3 +17,39 @@ public:
         return n+1;
     }
 };
+method 2:
+need to modify
+class Solution {
+public:
+    int segrate(vector<int> &arr,int n)
+    {   int j=0;
+        for(int i=0;i<n;i++)
+        { if(arr[i]<0)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j++;
+        }
+        }
+     return j;
+    }
+    int find_mising(vector<int>&arr,int n,int shift)
+    {
+        for(int i=shift;i<n;i++)
+        { if(arr[abs(arr[i])-1]>0 &&abs(arr[i])-1<n)
+            arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+                return i+1;
+        }
+        return n;
+    }
+    int firstMissingPositive(vector<int>& arr) {
+        int n=arr.size();
+        int shift=segrate(arr,n);
+        return find_mising(arr,n,shift);
+    }
+};
