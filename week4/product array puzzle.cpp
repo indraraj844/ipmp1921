b6f655a591fa7f14a00ae35a39e0bd758e692b75
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-14 22:37:50 +0530

    Create product array puzzle.cpp

---
+++ week4/product array puzzle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+      int n=arr.size();
+        int mul=1;
+        int flag=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!arr[i])flag++;
+            else
+            mul*=arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(flag>1)arr[i]=0;
+            else if(flag==0)
+            {
+                arr[i]=exp(log(mul)-log(arr[i]));
+            }
+            else if(flag==1 && arr[i]!=0) arr[i]=0;
+            else
+                arr[i]=mul;
+            
+        }
+        return arr;
+    }
+};
+need to make more efficent
