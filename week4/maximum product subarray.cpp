6fea021c11a68ad421f53f2fdc42f814ca40e472
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-15 12:13:25 +0530

    Update maximum product subarray.cpp

--- week4/maximum product subarray.cpp
+++ week4/maximum product subarray.cpp
@@ -20,3 +20,40 @@ public:
     }
 };
 method 2:
+class Solution {
+public:
+    int maxProduct(vector<int>& arr) {
+        int n=arr.size();
+        int max_end=1,min_end=1;
+        int max_prod=0;
+        int flag=0;
+        if(n==1)
+        {
+            return arr[0];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+            {
+            max_end*=arr[i];
+            min_end=min(1,min_end);
+                flag=1;
+            }
+            else if(arr[i]==0)
+            {
+                max_end=1;
+                min_end=1;
+            }
+            else
+            {
+                int temp=max_end;
+                max_end=max(1,min_end*arr[i]);
+                min_end=temp*arr[i];
+            }
+            max_prod=max(max_prod,max_end);
+        }
+            return max_prod;
+        
+    }
+};
+need to change
