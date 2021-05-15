5cf43c6a495c1696f8b5f0ff28e85bd44545adce
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-15 11:10:12 +0530

    Update product array puzzle.cpp

--- week4/product array puzzle.cpp
+++ week4/product array puzzle.cpp
@@ -26,3 +26,30 @@ public:
     }
 };
 need to make more efficent
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+      int n=arr.size();
+        int mul=1;
+        int flag=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!arr[i])flag++;
+            else
+            mul*=arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(flag>1)arr[i]=0;
+            else if(flag==0)
+            {
+                arr[i]=mul* pow(arr[i],-1);
+            }
+            else if(flag==1 && arr[i]!=0) arr[i]=0;
+            else
+                arr[i]=mul;
+            
+        }
+        return arr;
+    }
+};
