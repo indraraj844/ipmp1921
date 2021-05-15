d75264e0e48133dd3b158092323111cf15fac3a7
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-15 11:17:10 +0530

    Create rotate d element array.cpp

---
+++ week4/rotate d element array.cpp
@@ -0,0 +1,16 @@
+method 1;
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+        int n=arr.size();
+        for(int i=0;i<k;i++)
+        {
+            int temp=arr[n-1];
+            for(int j=n-1;j>0;j--)
+                arr[j]=arr[j-1];
+            
+            arr[0]=temp;
+        }
+    }
+};
+method 2:
