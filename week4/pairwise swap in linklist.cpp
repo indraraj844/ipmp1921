1a8194d4a1806ab1555fccf1adb3cc19f53feaf4
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-21 10:13:51 +0530

    Update pairwise swap in linklist.cpp

--- week4/pairwise swap in linklist.cpp
+++ week4/pairwise swap in linklist.cpp
@@ -35,3 +35,26 @@ public:
         return head;
     }
 };
+method 3 
+  recursive approch
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head ||!head->next)return head;
+        ListNode *next=head->next->next;
+        head->next->next=head;
+        ListNode* newhead=head->next;
+        head->next=swapPairs(next);
+        return newhead;
+    }
+};
