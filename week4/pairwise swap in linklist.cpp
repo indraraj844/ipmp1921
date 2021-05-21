0fbd9ac375f0e1d1b88314221ce930461ef7242b
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-05-21 10:07:37 +0530

    Create pairwise swap in linklist.cpp

---
+++ week4/pairwise swap in linklist.cpp
@@ -0,0 +1,37 @@
+method 1 
+  swaping data of nodes
+  method 2:
+change in  pointer:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head ||!head->next)return head;
+        ListNode *curr=head->next,*prev=head;
+          head=curr;
+           while(true)
+        {
+               ListNode* next=curr->next;
+               curr->next=prev;
+               if(!next ||!next->next)
+               {
+                prev->next=next;
+               break;
+               }
+               prev->next=next->next;
+               prev=next;
+               curr=prev->next;
+        
+        }
+        return head;
+    }
+};
