662f4769a171ff8424fed80237183023bc0894d9
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-04 11:24:15 +0530

    Create delete a node in bst.cpp

---
+++ week7/recursion/delete a node in bst.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* minvalnode(TreeNode* root)
+    {
+        TreeNode* curr=root;
+        while(curr && curr->left)
+            curr=curr->left;
+        
+        return curr;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)return root;
+        else if(root->val>key)
+            root->left=deleteNode(root->left,key);
+        else if(root->val<key)
+            root->right= deleteNode(root->right,key);
+        else
+        {
+            if(!root->right &&!root->left)return NULL;
+            else if(!root->right)
+            {
+                TreeNode* temp=root->left;
+                return temp;
+            }
+            else if(!root->left)
+            {
+                TreeNode* temp=root->right;
+                return temp;
+            }
+            
+          TreeNode* temp=minvalnode(root->right);
+            cout<<temp->val<<" ";
+            root->val=temp->val;
+           root->right=deleteNode(root->right,root->val);
+        }
+        return root;
+    }
+};
