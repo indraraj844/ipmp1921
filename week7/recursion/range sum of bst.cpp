937f798ec70b3a72163c4e95e9cc058cf1ff0435
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-06 22:18:39 +0530

    Update range sum of bst.cpp

--- week7/recursion/range sum of bst.cpp
+++ week7/recursion/range sum of bst.cpp
@@ -15,4 +15,39 @@ public:
         
     }
 };
+method 2
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, int low, int high,int& sum)
+    {
+        if(!root)return;
+        if(root->val>low)
+            helper(root->left,low,high,sum);
+        if(root->val<high)
+            helper(root->right,low,high,sum);
+        
+        if(root->val>=low && root->val<=high)
+            sum+=root->val;
+    }
+     
+        
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root)return 0;
+        int sum=0;
+        helper(root,low,high,sum);
+        return sum;
+        
+    }
+};
 thankyou
