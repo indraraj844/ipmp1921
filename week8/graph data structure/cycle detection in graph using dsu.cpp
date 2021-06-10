bf93a36e2c3afc87d6b7297c7efbbde9dbbd35ab
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-10 22:43:30 +0530

    Create cycle detection in graph using dsu.cpp

---
+++ week8/graph data structure/cycle detection in graph using dsu.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define N 1e5
+
+vector<int>parent(N);
+vector<int>sz(N);
+    
+    void make_set(int v)
+    {
+        parent[v]=v;
+        sz[v]=1;
+    }
+  int find_set(int v)
+  {
+      if(v==parent[v])
+      return v;
+      return parent[v]=find_set(parent[v]);
+  }
+   int union_set(int A,int B)
+   {
+       int a=find_set(A);
+       int b=find_set(B);
+       
+       if(a!=b)
+       {
+           if(sz[a]<sz[b])swap(a,b);
+           
+           parent[b]=a;
+           sz[a]+=sz[b];
+       }
+       
+   }
+int main()
+{
+    for(int i=0;i<N;i++)
+      make_set(i);
+      
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>edge;
+    
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        edge.push_back({u,v});
+    }
+    bool cycle=false;
+    for(auto p:edge)
+    {
+        int u=p[0];
+        int v=p[1];
+        int x=find_set(u);
+        int y=find_set(v);
+        
+        if(x==y)
+        {
+            cycle=true;
+            break;
+        }
+        else
+        {
+            union_set(x,y);
+        }
+        
+    }
+    if(cycle)
+    cout<<"cycle is found";
+    else
+    cout<<" cycle not found";
+    
+}
