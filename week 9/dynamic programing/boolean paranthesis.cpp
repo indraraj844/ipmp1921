3d2d364df4756af3e36061f5ce44032efdaf18fc
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-17 23:18:35 +0530

    Create boolean paranthesis.cpp

---
+++ week 9/dynamic programing/boolean paranthesis.cpp
@@ -0,0 +1,61 @@
+int helper(string symb,string oper,int n)
+    {
+    int F[n][n], T[n][n];
+    for (int i = 0; i < n; i++) {
+        F[i][i] = (symb[i] == 'F') ? 1 : 0;
+        T[i][i] = (symb[i] == 'T') ? 1 : 0;
+    }
+    for (int gap = 1; gap < n; ++gap)
+    {
+        for (int i = 0, j = gap;
+             j < n; ++i, ++j)
+        {
+            T[i][j] = F[i][j] = 0;
+            for (int g = 0;
+                 g < gap; g++)
+            {
+                int k = i + g;
+
+                int tik = T[i][k] + F[i][k];
+                int tkj = T[k + 1][j]
+                  + F[k + 1][j];
+ 
+                if (oper[k] == '&') {
+                    T[i][j] += T[i][k]
+                            * T[k + 1][j];
+                    F[i][j] += (tik * tkj
+                                - T[i][k]
+                                * T[k + 1][j]);
+                }
+                if (oper[k] == '|') {
+                    F[i][j] += F[i][k]
+                      * F[k + 1][j];
+                    T[i][j] += (tik * tkj
+                                - F[i][k]
+                                * F[k + 1][j]);
+                }
+                if (oper[k] == '^') {
+                    T[i][j] += F[i][k]
+                      * T[k + 1][j]
+                      + T[i][k]
+                      * F[k + 1][j];
+                    F[i][j] += T[i][k]
+                            * T[k + 1][j]
+                            + F[i][k] * F[k + 1][j];
+                }
+            }
+        }
+    }
+    return T[0][n - 1];
+}
+    int countWays(int n, string str){
+     
+     string op="",st="";
+     for(int i=0;i<n;i++)
+     {
+         if(i%2)op+=str[i];
+         else st+=str[i];
+     }
+     return helper(st,op,n/2+1);
+    
+    }
