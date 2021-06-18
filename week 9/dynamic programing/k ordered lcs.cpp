5f63fa563a22a4cd0e33387a302cb506fa9702ed
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-18 15:52:19 +0530

    Create k ordered lcs.cpp

---
+++ week 9/dynamic programing/k ordered lcs.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 10
+
+int lcs(int arr1[], int n,int arr2[], int m, int k)
+{
+    int dp[n+1][m+1][k+1];
+    for(int i=0;i<n+1;i++)
+   {
+     for(int j=0;j<m+1;j++)
+    {
+     for(int t=0;t<k+1;t++)
+     {
+         if(i==0||j==0)dp[i][j][t]=0;
+        else
+        {
+          int ans=max(dp[i-1][j][t],dp[i][j-1][t]);
+          if(arr1[i-1]==arr2[j-1])
+           ans=max(ans,1+dp[i-1][j-1][t]);
+           else
+           {
+               if(t>0)
+              ans=max(ans,1+dp[i-1][j-1][t-1]); 
+              else
+              ans=max(ans,dp[i-1][j-1][t]);
+           }
+           
+           
+           dp[i][j][t]=ans;
+        }
+     }
+    }
+   }
+   return dp[n][m][k];
+}
+int main()
+{
+	int k = 2;
+	int arr1[] = { 1, 3, 2, 4, 5 };
+	int arr2[] = { 1, 3, 1,  4 ,4};
+	int n = sizeof(arr1) / sizeof(arr1[0]);
+	int m = sizeof(arr2) / sizeof(arr2[0]);
+	cout << lcs( arr1, n, arr2, m, k) << endl;
+
+	return 0;
+}
+thankyou
