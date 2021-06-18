629f993e0c95d87552c8fa377110c5cb5e98902a
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-18 11:44:48 +0530

    Update word break problem.cpp

--- week 9/dynamic programing/word break problem.cpp
+++ week 9/dynamic programing/word break problem.cpp
@@ -45,3 +45,38 @@ int wordBreak(string str, vector<string> &b) {
    }
     return dp[0];
 }
+other logic for dynamic programing
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>&dict) {
+    
+        if(dict.size() == 0)
+            return false;
+        int n = s.size();
+        bool t[n + 1];
+        memset(t, false, sizeof(t));
+        t[0] = true;
+        
+         for(int i = 1; i <= s.size(); i++)
+        {
+            for(int j = i - 1;j >= 0; j--)
+            {
+                if(t[j])
+                {
+                    string str = s.substr(j, i - j);
+					auto it = find(dict.begin(), dict.end(), str);  
+                    if(it != dict.end())
+                    {
+                        t[i] = true;
+                        break; 
+                    }
+                }
+            }
+        }
+        
+        return t[n];
+    }   
+    
+};
+thankyou
+
