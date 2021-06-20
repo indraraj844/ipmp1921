80a7e920c33d0be8d3037205eba402ca5aeb81b9
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-06-20 22:48:35 +0530

    Update longest increasing subsequance.cpp

--- week 9/dynamic programing/longest increasing subsequance.cpp
+++ week 9/dynamic programing/longest increasing subsequance.cpp
@@ -16,3 +16,20 @@ int longestSubsequence(int n, int a[])
 need to improve o(n^2)->o(nlogn)
      
   
+method 2 nlogn
+ int longestSubsequence(int n, int a[])
+    {
+      vector<int>s;
+      s.push_back(a[0]);
+      for(int i=1;i<n;i++)
+      {
+          if(a[i]>s.back())s.push_back(a[i]);
+          else
+          {
+            int ind=lower_bound(s.begin(),s.end(),a[i])-s.begin();
+            s[ind]=a[i];
+          }
+      }
+      return s.size();
+    }
+thankyou
