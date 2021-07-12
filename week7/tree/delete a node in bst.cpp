7254b4964ac350fc65d04e6a7932f587915ecd3a
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-07-12 17:30:07 +0530

    Update delete a node in bst.cpp

--- week7/tree/delete a node in bst.cpp
+++ week7/tree/delete a node in bst.cpp
@@ -36,4 +36,58 @@ public:
         return root;
     }
 };
+method 2 somewhat optimize
+
+class Solution {
+public:
+   
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)return root;
+      if(root->val>key)
+      {
+          
+          root->left=deleteNode(root->left,key);
+          return root;
+          
+      }
+        else if(root->val<key)
+        {
+             root->right=deleteNode(root->right,key);
+            return root;
+        }
+            
+        else
+        {
+            if(!root->left && !root->right)return NULL;
+            else if( !root->right)
+            {
+               return root->left;
+            }
+             else if( !root->left)
+            {
+               return root->right;
+            }
+            else
+            {
+              TreeNode* succparent=root;
+              TreeNode* succ=root->right;
+              
+                while(succ->left)
+                {
+                    succparent=succ;
+                    succ=succ->left;
+                }
+                
+                if(succparent!=root)
+                  succparent->left=succ->right;  
+                else
+                    succparent->right=succ->right;
+                
+                root->val=succ->val;
+                delete(succ);
+                return root;
+            }
+        }
+    }
+};
 thankyou
