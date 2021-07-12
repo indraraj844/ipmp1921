c721921bc53a896c211dee907176fb2849da4dfe
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-07-12 17:13:33 +0530

    Update delete a node in bst.cpp

--- week7/tree/delete a node in bst.cpp
+++ week7/tree/delete a node in bst.cpp
@@ -1,51 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    TreeNode* minvalnode(TreeNode* root)
+    TreeNode* precedure(TreeNode* root)
     {
-        TreeNode* curr=root;
-        while(curr && curr->left)
-            curr=curr->left;
+        TreeNode* root1=root;
+        while(root1->left)
+           root1= root1->left;
         
-        return curr;
+        return root1;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root)return root;
-        else if(root->val>key)
-            root->left=deleteNode(root->left,key);
+      if(root->val>key)
+          root->left=deleteNode(root->left,key);
         else if(root->val<key)
-            root->right=deleteNode(root->right,key);
+             root->right=deleteNode(root->right,key);
         else
         {
-            if(!root->right &&!root->left)return NULL;
-            else if(!root->right)
+            if(!root->left && !root->right)return NULL;
+            else if(root->left&& !root->right)
             {
-                TreeNode* temp=root->left;
-                free(root);
-                return temp;
+               return root->left;
             }
-            else if(!root->left)
+             else if(root->right&& !root->left)
             {
-                TreeNode* temp=root->right;
-                free(root);
-                return temp;
+               return root->right;
             }
-            
-          TreeNode* temp=minvalnode(root->right);
-            root->val=temp->val;
-           root->right=deleteNode(root->right,temp->val);
+            else
+            {
+              TreeNode* prec=precedure(root->right);
+                root->val=prec->val;
+              root->right= deleteNode(root->right,root->val);
+            }
         }
         return root;
     }
 };
-need to change
+thankyou
