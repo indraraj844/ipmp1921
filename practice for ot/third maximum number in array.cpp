400a3fbf4b8ca89bf0e5a09ff6b9684b3c1a2487
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-07-23 10:11:16 +0530

    Update third maximum number in array.cpp

--- practice for ot/third maximum number in array.cpp
+++ practice for ot/third maximum number in array.cpp
@@ -2,41 +2,36 @@ method 1 using sorting the array o(nlogn)
 class Solution {
 public:
     int thirdMax(vector<int>& arr) {
-        int n=arr.size();
-        if(n==1)return arr[0];
-        if(n==2)return arr[0]>arr[1]?arr[0]:arr[1];
+      int n=arr.size();
+      if(n<3)return *max_element(arr.begin(),arr.end());
         
-        sort(arr.begin(),arr.end());
-        int k=3;
-        int i;
-        for( i=n-1;i>=1;i--)
-        {
-            if(arr[i]>arr[i-1])
-                k--;
-            if(k==0)
-                return arr[i];
-        }
-        if(k==1&& i==0)return arr[0];
-        return arr[n-1];
-    }
+      sort(arr.begin(),arr.end());
+        int k=2;
+        int f=arr[n-1];
+      for(int i=n-2;i>-1;i--)
+      {
+          if(arr[i]!=f)
+          {
+              f=arr[i];
+              k--;
+          }
+          if(k==0)return f;
+      }
+      return arr[n-1];
+        
+    }   
 };
 method 2using a ordered set
 
   class Solution {
 public:
     int thirdMax(vector<int>& arr) {
-        set<int,greater<int>>s;
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-            s.insert(arr[i]);
-        
-        set<int,greater<int> >::iterator itr;
-         itr=s.begin();
-        if(s.size()<3)
-            return *itr;
-        advance(itr,2);
-        return *itr;
-    }
+        set<int,greater<int>>s(arr.begin(),arr.end());
+          set<int,greater<int>>::iterator it=s.begin();
+        if(s.size()<3)return *it;
+        it++;it++;
+        return *it;
+    }   
 };
 method 3 best without extra space o(n)
   class Solution {
