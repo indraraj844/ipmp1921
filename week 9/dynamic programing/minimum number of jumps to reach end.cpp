fb5b8d6a335dd0317e7e256b144d9acc8c319606
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-07-31 11:14:42 +0530

    Update minimum number of jumps to reach end.cpp

--- week 9/dynamic programing/minimum number of jumps to reach end.cpp
+++ week 9/dynamic programing/minimum number of jumps to reach end.cpp
@@ -21,3 +21,32 @@ public:
         return dp[n-1];
     }
 };
+method 2 o(n)
+    class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1)return 0;
+        
+        int ladder=nums[0];  //ladder size
+        int stairs=nums[0]; //how many stairs in ladder
+        int jump=1;
+        for(int level=1;level<n;level++)
+        {
+            if(level==n-1)return jump; // i reached at end
+            
+            else if(level+nums[level]>ladder)
+                 ladder=level+nums[level];  //getting max ladder
+            
+            stairs--; // in every ladder the stair will be less by 1
+            if(stairs==0)
+            {
+                jump++;
+                stairs=ladder-level; //travel for remaining ladder
+            }
+        }
+        return jump;
+        
+    }
+};
+thankyou
