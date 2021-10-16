cebf60a68bfb6f75a26a028a68450ffd4292209f
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-10-16 14:38:51 +0530

    Update multiply two string.cpp

--- practice for ot/multiply two string.cpp
+++ practice for ot/multiply two string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string multiply(string s1, string s2) {
-        if(s1=="0" || s2=="0") //if any of the two strings is zero then multiplication will be zero
+        if(s1=="0" || s2=="0") 
             return "0";
         int n=s1.length();
         int m=s2.length();
@@ -11,24 +11,14 @@ public:
         {
             for(int i=n-1;i>=0;i--)
             {
-               
-                 ans[i+j+1]+=(s2[j]-'0')*(s1[i]-'0');// doing s2[j]-'0' to convert char to int
-                
-            //Adding the previous number present at ans[i+j+1] with multiplication of two string 
-             //if carry is generated it will be resolved in the below k loop
-            
+                 ans[i+j+1]+=(s2[j]-'0')*(s1[i]-'0');
             }
-            
-            //now ans[i] can max contain double digit number(as 9*9 is 81) so now we are taking the carry out and adding it to ans[i-1]
             // eg-[81,81,81]----->[8,9,9,1]
             for(int k=ans.size()-1;k>0;k--)
             {
                 int num=ans[k];
-                
-                ans[k]=num%10; // i and j are starting from end so numbers in ans will be alloted from last 
-                                //digit will be on right( one position ahead) and carry will be on left(one position less)                                                         // so that carry is added in further calculation
-                
-                ans[k-1]+=num/10;   //carry at given position
+                ans[k]=num%10; 
+                ans[k-1]+=num/10;
             }
         }
         
